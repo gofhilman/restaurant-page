68fab00a39f53ba76676654ea4b59a465c3e2f2a
Hilman Fikry <hilman12fikry@gmail.com>
2025-03-18 21:59:29 +0700

    Complete menu page structure

--- src/menu.js
+++ src/menu.js
@@ -6,68 +6,79 @@ import dinnerOne from "./dinner-1.png";
 import dinnerTwo from "./dinner-2.png";
 
 const containers = [];
-const containerTitles = [];
-const food = [];
+
+const menuSections = ["Breakfast", "Lunch", "Dinner"];
+
+const imageSrc = [
+    breakfastOne, breakfastTwo, lunchOne,
+    lunchTwo, dinnerOne, dinnerTwo
+];
+
+const imageAlt = [
+    "Baked Egg with Spinach", "Rustic Country Omelette",
+    "Pineapple Chicken Burger", "Bulk-Up Tuna Nicoise",
+    "Classic Steak and Eggs", "Japanese Beef and Wasabi Mash"
+];
+
+const infoGrids = [];
+const number = ['206', '9g', '8g', '22g', '288', '11g', '13g', '34g',
+    '460', '12g', '45g', '45g', '594', '16g', '25g', '52g',
+    '725', '25g', '2g', '87g', '409', '9g', '32g', '30g'];
+const sectionText = ['Calories', 'Fat', 'Carbs', 'Protein'];
+let numberIter = 0;
+let textIter = 0;
+for (let i = 0; i < 6; i++) {
+    const infoGrid = document.createElement('div');
+    for (let j = 0; j < 2; j++) {
+        for (let k = 0; k < 2; k++) {
+            const infoNumber = document.createElement('h2');
+            infoNumber.classList.add('info-number');
+            infoNumber.textContent = number[numberIter++];
+            infoGrid.appendChild(infoNumber);
+        }  
+        for (let k = 0; k < 2; k++) {
+            const infoText = document.createElement('h2');
+            infoText.classList.add('info-text');
+            infoText.textContent = sectionText[textIter%4];
+            textIter++;
+            infoGrid.appendChild(infoText);
+        } 
+    }
+    infoGrids.push(infoGrid);   
+}
 
 for (let i = 0; i < 3; i++) {
     const container = document.createElement('div');
     container.classList.add('container');
     const containerTitle = document.createElement('p');
     containerTitle.classList.add('container-title', 'marker');
+    containerTitle.textContent = menuSections[i];
     container.appendChild(containerTitle);
     const containerContent = document.createElement('div');
     containerContent.classList.add('container-content');
-    let subFood = [];
+    
     for (let j = 0; j < 2; j++) {
         const menu = document.createElement('div');
         menu.classList.add('menu');
-        let subSubFood = [];
         const foodImage = document.createElement('img');
         foodImage.classList.add('food-image');
+        Object.assign(foodImage, {
+            src: imageSrc[2*i+j],
+            alt: imageAlt[2*i+j],
+            width: 400
+        });
         const foodName = document.createElement('h1');
         foodName.classList.add('food-name');
-        const foodInfo = document.createElement('h2');
+        foodName.textContent = imageAlt[2*i+j];
+        const foodInfo = infoGrids[2*i+j];
         foodInfo.classList.add('food-info');
         menu.append(foodImage, foodName, foodInfo);
         containerContent.appendChild(menu);
-        subSubFood.push(foodImage, foodName, foodInfo);
-        subFood.push(subSubFood);
     }
     container.appendChild(containerContent);
     containers.push(container);
-    containerTitles.push(containerTitle);
-    food.push(subFood);
 }
 
 const [breakfastContainer, lunchContainer, dinnerContainer] = containers;
-const [breakfastTitle, lunchTitle, dinnerTitle] = containerTitles;
-const [[[firstBreakfastImage, firstBreakfastTitle, firstBreakfastInfo], 
-    [secondBreakfastImage, secondBreakfastTitle, secondBreakfastInfo]],
-    [[firstLunchImage, firstLunchTitle, firstLunchInfo], 
-    [secondLunchImage, secondLunchTitle, secondLunchInfo]],
-    [[firstDinnerImage, firstDinnerTitle, firstDinnerInfo],
-    [secondDinnerImage, secondDinnerTitle, secondDinnerInfo]]] = food;
-
-breakfastTitle.textContent = "Breakfast";
-lunchTitle.textContent = "Lunch";
-dinnerTitle.textContent = "Dinner";
-
-const images = [firstBreakfastImage, secondBreakfastImage, firstLunchImage,
-    secondLunchImage, firstDinnerImage, secondDinnerImage];
-
-const imageSrc = [breakfastOne, breakfastTwo, lunchOne,
-    lunchTwo, dinnerOne, dinnerTwo];
-
-const imageAlt = ["Baked Egg with Spinach", "Rustic Country Omelette",
-    "Pineapple Chicken Burger", "Bulk-Up Tuna Nicoise",
-    "Classic Steak and Eggs", "Japanese Beef and Wasabi Mash"];
-
-for (let i = 0; i < images.length; i++) {
-    Object.assign(images[i], {
-        src: imageSrc[i],
-        alt: imageAlt[i],
-        width: 400
-    });
-}
 
 export { breakfastContainer, lunchContainer, dinnerContainer };
